a1933df94473a9f690e4ddae4209be73ec4bb721
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-11-18 17:44:07 -0500

    Sixth commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import nba from 'nba';
-import { ShotChart } from './ShotChart';
 import { Profile } from './Profile';
+import { DataViewContainer } from './DataViewContainer';
 
 export class Main extends React.Component {
   state = {
@@ -10,20 +10,20 @@ export class Main extends React.Component {
   }
 
   componentDidMount() {
-    nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
-      const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-      console.log('playerInfo', playerInfo);
-      this.setState({
-        playerInfo: playerInfo,
-      });
-    });
+    nba.stats.playerInfo({ PlayerID: this.state.playerId })
+      .then((info) => {
+        const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+        console.log('playerInfo', playerInfo);
+        this.setState({ playerInfo });
+      })
+      .catch((e) => console.log(e))
   }
 
   render() {
     return (
       <div className="main">
-        <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
-        <ShotChart playerId={this.state.playerId}/>
+        <Profile playerInfo={this.state.playerInfo} />
+        <DataViewContainer playerId={this.state.playerId}/>
       </div>
     );
   }
